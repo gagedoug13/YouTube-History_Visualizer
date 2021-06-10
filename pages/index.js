7c03836db9928a6fb1c4ed9d714391a6bdd85b1f
Gage Thornberry <gagedoug13@gmail.com>
2021-06-10 12:16:30 -0700

    feature(index component): mostly completed home page. working on charts page next.

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,36 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='homeDiv' > 
-                    
-      <div className='welcomeDiv'>
-        <h1 className='welcome'>Welcome to YouTube Data Visualizer</h1>
+    <div className={styles.homeDiv} >             
+      <div className={styles.welcomeDiv}>
+        <h1 className={styles.welcome}>Welcome to YouTube Data Visualizer</h1>
       </div>
     
-      <div className='fileInstructionsDiv'>
+      <div className={styles.fileInstructionsDiv}>
         <input 
-            className='fileButton' 
+            className={styles.fileButton} 
             type="file" 
             accept='.json' 
             id="myFile" 
             name="filename"
         />
                 
-        <h3 className='or'>or</h3>
+        <h3 className={styles.or}>or</h3>
         <Link href='/instructions'>
-          <a><button className='basicInstructionsButton'>instructions</button></a>
+          <a><button className={styles.basicInstructionsButton}>instructions</button></a>
         </Link>
 
       </div>
     
-      <div className='basicInstructionsDiv'>
-        <p className='howTitle'>how it works</p>
-        <h3 className='basicInstructions'>You download your raw <span style={{color: "red"}}>YouTube</span> data from <a className='googleLink' href='https://google.takeouts.com'>google.takeouts.com</a> and we 
+      <div className={styles.basicInstructionsDiv}>
+        <p className={styles.howTitle}>how it works</p>
+        <h3 className={styles.basicInstructions}>You download your raw <span style={{color: "red"}}>YouTube</span> data from <a className='googleLink' href='https://google.takeouts.com'>google.takeouts.com</a> and we 
           visualize it for you.</h3>
     
-        <h3 className='basicInstructions'>All you have to do is follow the instructions 
+        <h3 className={styles.basicInstructions}>All you have to do is follow the instructions 
           above and you'll be able to see your most viewed channels, videos, 
           and key words from your YouTube history.</h3>
       </div>
--- pages/instructions.js
+++ pages/instructions.js
@@ -7,42 +7,42 @@ export default function Instructions() {
         <div>
             <div className={styles.instructionsCard}>
                 <h2 className={styles.instructionsBody}>Step 1. go to <a target='_blank' href='https://takeout.google.com/'>takeout.google.com</a> to download your youtube history.</h2>
-                <Image src='/instructions/google-takeout.png' alt="me" width={850} height={420} />
+                <Image rel="preload" src='/instructions/google-takeout.png' alt="me" width={850} height={420} />
             </div>
 
             <div className={styles.instructionsCard}>
                 <h2 className={styles.instructionsBody}>Scroll down and hit deselect all to uncheck all of the chosen data. </h2>
-                <Image src='/instructions/deselect-all.png' alt="me" width={850} height={420}/>
+                <Image rel="preload" src='/instructions/deselect-all.png' alt="me" width={850} height={420}/>
             </div>
 
             <div className={styles.instructionsCard}>
                 <h2 className={styles.instructionsBody}>Now go to the bottom and hit the checkmark next to the YouTube icon. Next, click on Multiple Formats under the YouTube button. </h2>
-                <Image src='/instructions/select-youtube.png' alt="me" width={850} height={420}/>
+                <Image rel="preload" src='/instructions/select-youtube.png' alt="me" width={850} height={420}/>
             </div>
 
             <div className={styles.instructionsCard}>
                 <h2 className={styles.instructionsBody}>Under the History tab, change the format from HTML to JSON and hit OK.</h2>
-                <Image src='/instructions/conversion.png' alt="me" width={850} height={420}/>
+                <Image rel="preload" src='/instructions/conversion.png' alt="me" width={850} height={420}/>
             </div> 
 
             <div className={styles.instructionsCard}>
                 <h2 className={styles.instructionsBody}>Hit the <span>Next step</span> button and then scroll down and hit <span>Create export</span>.</h2>
-                <Image src='/instructions/create-export.png' alt="me" width={850} height={420}/>
+                <Image rel="preload" src='/instructions/create-export.png' alt="me" width={850} height={420}/>
             </div>
 
             <div className={styles.instructionsCard}>
                 <h2 className={styles.instructionsBody}>When the data is ready, go to the email from Google and download your files.</h2>
-                <Image src='/instructions/download-your-files.png' alt="me" width={850} height={420}/>
+                <Image rel="preload" src='/instructions/download-your-files.png' alt="me" width={850} height={420}/>
             </div>
 
             <div className={styles.instructionsCard}>
                 <h2 className={styles.instructionsBody}>In the location that you saved your data, go to the zip file and double-click takeout / youtube and youtube music / history/ and at the top of the page hit extract all.</h2>
-                <Image src='/instructions/extract-all.png' alt="me" width={850} height={420}/>
+                <Image rel="preload" src='/instructions/extract-all.png' alt="me" width={850} height={420}/>
             </div>
 
             <div className={styles.instructionsCard}>
                 <h2 className={styles.instructionsBody}>Its time to upload your data. When choosing the file, go to Takeout / YouTube and YouTube Music / History / Watch History.</h2>
-                <Image src='/instructions/zip-file.png' alt="me" width={850} height={420}/>
+                <Image rel="preload" src='/instructions/zip-file.png' alt="me" width={850} height={420}/>
             </div>
         </div>
     )
