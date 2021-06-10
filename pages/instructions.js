5949cc7d270c4a21d6fdc86a0041c7879fac942e
Gage Thornberry <gagedoug13@gmail.com>
2021-06-10 11:31:08 -0700

    feature(instructions component): built out instructions page and am working on home page now

--- pages/instructions.js
+++ pages/instructions.js
@@ -1,55 +1,49 @@
 import React from 'react'
 import Image from 'next/image'
-// import googleTakeout from 'next/google-takeout.png'
-// import deselect from '../instructionPhotos/deselect-all.png'
-// import selectYoutube from '../instructionPhotos/select-youtube.png'
-// // import conversion from '../instructionPhotos/conversion.png'
-// import createExport from '../instructionPhotos/create-export.png'
-// import downloadYourFiles from '../instructionPhotos/download-your-files.png'
-// import extractAll from '../instructionPhotos/extract-all.png'
+import styles from '../styles/instructions.module.css'
 
 export default function Instructions() {
     return (
         <div>
-            <div className='instructionsCard'>
-                <h2 className='instructionsBody'>Step 1. go to <a target='_blank' href='https://takeout.google.com/'>takeout.google.com</a> to download your youtube history.</h2>
-                <Image className='instructionsPhoto' src='/google-takeout.png' alt="me" width="64" height="64"/>
+            <div className={styles.instructionsCard}>
+                <h2 className={styles.instructionsBody}>Step 1. go to <a target='_blank' href='https://takeout.google.com/'>takeout.google.com</a> to download your youtube history.</h2>
+                <Image src='/instructions/google-takeout.png' alt="me" width={850} height={420} />
             </div>
 
-            {/* <div className='instructionsCard'>
-                <h2 className='instructionsBody'>Scroll down and hit deselect all to uncheck all of the chosen data. </h2>
-                <img className='instructionsPhoto' src={deselect}></img>
+            <div className={styles.instructionsCard}>
+                <h2 className={styles.instructionsBody}>Scroll down and hit deselect all to uncheck all of the chosen data. </h2>
+                <Image src='/instructions/deselect-all.png' alt="me" width={850} height={420}/>
             </div>
 
-            <div className='instructionsCard'>
-                <h2 className='instructionsBody'>Now go to the bottom and hit the checkmark next to the YouTube icon. Next, click on Multiple Formats under the YouTube button. </h2>
-                <img className='instructionsPhoto' src={selectYoutube}></img>
+            <div className={styles.instructionsCard}>
+                <h2 className={styles.instructionsBody}>Now go to the bottom and hit the checkmark next to the YouTube icon. Next, click on Multiple Formats under the YouTube button. </h2>
+                <Image src='/instructions/select-youtube.png' alt="me" width={850} height={420}/>
             </div>
 
-            <div className='instructionsCard'>
-                <h2 className='instructionsBody'>Under the History tab, change the format from HTML to JSON and hit OK.</h2>
-                <img className='instructionsPhoto' src={conversion}></img>
+            <div className={styles.instructionsCard}>
+                <h2 className={styles.instructionsBody}>Under the History tab, change the format from HTML to JSON and hit OK.</h2>
+                <Image src='/instructions/conversion.png' alt="me" width={850} height={420}/>
             </div> 
 
-            <div className='instructionsCard'>
-                <h2 className='instructionsBody'>Hit the <span>Next step</span> button and then scroll down and hit <span>Create export</span>.</h2>
-                <img className='instructionsPhoto' src={createExport}></img>
+            <div className={styles.instructionsCard}>
+                <h2 className={styles.instructionsBody}>Hit the <span>Next step</span> button and then scroll down and hit <span>Create export</span>.</h2>
+                <Image src='/instructions/create-export.png' alt="me" width={850} height={420}/>
             </div>
 
-            <div className='instructionsCard'>
-                <h2 className='instructionsBody'>When the data is ready, go to the email from Google and download your files.</h2>
-                <img className='instructionsPhoto' src={downloadYourFiles}></img>
+            <div className={styles.instructionsCard}>
+                <h2 className={styles.instructionsBody}>When the data is ready, go to the email from Google and download your files.</h2>
+                <Image src='/instructions/download-your-files.png' alt="me" width={850} height={420}/>
             </div>
 
-            <div className='instructionsCard'>
-                <h2 className='instructionsBody'>In the location that you saved your data, go to the zip file and double-click takeout / youtube and youtube music / history/ and at the top of the page hit extract all.</h2>
-                <img className='instructionsPhoto' src={extractAll}></img>
+            <div className={styles.instructionsCard}>
+                <h2 className={styles.instructionsBody}>In the location that you saved your data, go to the zip file and double-click takeout / youtube and youtube music / history/ and at the top of the page hit extract all.</h2>
+                <Image src='/instructions/extract-all.png' alt="me" width={850} height={420}/>
             </div>
 
-            <div className='instructionsCard'>
-                <h2 className='instructionsBody'>Its time to upload your data. When choosing the file, go to Takeout / YouTube and YouTube Music / History / Watch History.</h2>
-                <img className='instructionsPhoto' src={extractAll}></img>
-            </div> */}
+            <div className={styles.instructionsCard}>
+                <h2 className={styles.instructionsBody}>Its time to upload your data. When choosing the file, go to Takeout / YouTube and YouTube Music / History / Watch History.</h2>
+                <Image src='/instructions/zip-file.png' alt="me" width={850} height={420}/>
+            </div>
         </div>
     )
 }
